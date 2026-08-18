1971005b50ba4e1e6a763158ec406ccf5906874a
Aviral Yadav <24f3000436@ds.study.iitm.ac.in>
2026-08-19 00:08:17 +0530

    Update: Emoji's available

--- backend/src/tasks/dto/comment.dto.ts
+++ backend/src/tasks/dto/comment.dto.ts
@@ -1,5 +1,9 @@
-import { IsString, MinLength } from 'class-validator';
+import { IsString, MaxLength, MinLength } from 'class-validator';
 
 export class CreateCommentDto {
   @IsString() @MinLength(1) content: string;
+}
+
+export class ReactDto {
+  @IsString() @MaxLength(8) emoji: string;
 }
